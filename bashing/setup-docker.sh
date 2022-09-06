695fad839be09ee96904c9061b3c138f63c40228
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2022-09-06 07:21:42 +0530

    011. 6.10 Using Here Documents

---
+++ bashing/setup-docker.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# script that runs 
+# https://kubernetes.io/docs/setup/production-environment/container-runtime
+
+yum install -y vim yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# notice that only verified versions of Docker may be installed
+# verify the documentation to check if a more recent version is available
+
+yum install -y docker-ce
+[ ! -d /etc/docker ] && mkdir /etc/docker
+
+
+# generate a file with name /etc/docker/daemon.json. so we see a small modification in the here-docs this time we have a cat greater then,
+# that is the output redirection the output redirection writes the file daemon.json and then we have the <<EOF
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable docker
+
+systemctl disable --now firewalld
